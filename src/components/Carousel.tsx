903139760cb029d346001b4215946ef3c1aeca89
setery <root@Nattalycbookair.lan>
2025-05-26 13:58:43 +1000

    fix:carrousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Carousel = () => {
   const sliderSettings = {
@@ -13,21 +13,51 @@ const Carousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-  }
+  };
 
   return (
     <Slider {...sliderSettings}>
       <div>
-        <img src="https://res.cloudinary.com/dhrvenmlj/image/upload/v1748230605/IMG_8552_nfar7z.heic" className="rounded-xl w-full h-[400px] object-cover" alt="Proyecto 1" />
+        <img
+          src="https://res.cloudinary.com/dhrvenmlj/image/upload/v1748230605/IMG_8552_nfar7z.heic"
+          alt="Proyecto"
+          style={{
+            width: "100%",
+            height: "300px",
+            objectFit: "cover",
+            objectPosition: "center top", // centra hacia arriba
+            borderRadius: "16px",
+          }}
+        />
       </div>
       <div>
-        <img src="https://res.cloudinary.com/dhrvenmlj/image/upload/v1748230605/IMG_8552_nfar7z.heic" className="rounded-xl w-full h-[400px] object-cover" alt="Proyecto 2" style={{ objectFit: 'contain', objectPosition: 'center' }}/>
+        <img
+          src="https://res.cloudinary.com/dhrvenmlj/image/upload/v1748230548/IMG_2312_gxfway.jpg"
+          alt="Proyecto"
+          style={{
+            width: "100%",
+            height: "300px",
+            objectFit: "cover",
+            objectPosition: "center top", // centra hacia arriba
+            borderRadius: "16px",
+          }}
+        />
       </div>
       <div>
-        <img style={{ objectFit: 'contain', objectPosition: 'center' }}  src="https://res.cloudinary.com/dhrvenmlj/image/upload/v1748230605/IMG_8552_nfar7z.heic" className="rounded-xl w-full h-[400px] object-cover" alt="Proyecto 3" />
+        <img
+          src="https://res.cloudinary.com/dhrvenmlj/image/upload/v1748230605/IMG_8519_jnw4ab.heic"
+          alt="Proyecto"
+          style={{
+            width: "100%",
+            height: "300px",
+            objectFit: "cover",
+            objectPosition: "center top", // centra hacia arriba
+            borderRadius: "16px",
+          }}
+        />
       </div>
     </Slider>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
